fbfb8ee645f488dd0605ddd1fa6fce4f6ae08367
afonsolpjr <afonsolpjr@hotmail.com>
2022-08-03 22:21:41 -0300

    Coloquei as funcoes que usam GMP no cripto.c

--- 2022.1/Num. Inteiros e Criptografia/cripto.c
+++ 2022.1/Num. Inteiros e Criptografia/cripto.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <gmp.h> 
+#include <time.h>
 
 
 long int* divisao();
@@ -20,7 +22,13 @@ long int valida_int_fatoracao(void);
 long int potencia(long int base,int exp);
 int checa_primalidade(long int n);
 
-
+void teste1(void);
+int teste_fermat(mpz_t numero, int base_i);
+void teste2(void);
+int teste_car(mpz_t numero);
+void exibir_pseudoprimos(void);
+void teste3();
+int miller_rabin(mpz_t numero,int base_i);
 
 int main(void)
 {
@@ -36,7 +44,11 @@ int main(void)
 		"[5] Fatoracao por Fermat\n"
 		"[6] Para a fatoraçao distinta\n"
 		"[7] QUantidade de divisores de um numero e soma dos divisores\n"
-		"[8] Numeros de Mersenne X Fermat, inserindo n \n"
+		"[8] Teste de Fermat\n"
+		"[9] Teste para ver se um numero é de  Carmichael\n"
+		"[10] TEste de Miller-Rabin para o numero dado.\n"
+        "[11] EXibe pseudoprimos para o numero dado.\n"
+		"[12] "
 		"[0] Para sair do programa");
 		opcao=valida_int();
 
@@ -109,12 +121,21 @@ int main(void)
 				break;
 
 			case 8:
-				puts("\n\n[*~*~*~*~*~*~]\nInsira o número n:");
-				a=valida_int_fatoracao();
-				numeros_perfeitos(a);
+				teste1();
+				break;
+		
+			case 9:
+				teste2();
 				break;
 
 
+            case 10:
+            teste3();
+            break;
+
+            case 11:
+            exibir_pseudoprimos();
+            break;
 		}
 	}
 
@@ -347,8 +368,13 @@ long int qtd_divisores(long int a)
 
 
 	//armazenando a soma da pg onde (razao=fator primo, termos = n_expoentes, 1o termo=1) para cada fator primo EM soma_pg
-	print_vetor(distintos,distintos[0]);
-	print_vetor(expoentes,distintos[0]);
+	
+	// print_vetor(distintos,distintos[0]);
+	// for(i=0;i<distintos[0]-1;i++)
+	// {
+	// 	printf("\no expoente de %d é = %d",distintos[i+1],expoentes[i]-1);
+	// }
+
 	for ( i=1,j=0; i < distintos[0]; i++,j++)
 	{
 		soma_pg[j]= ( potencia(distintos[i],expoentes[j]) - 1 ) / ( distintos[i] - 1);
@@ -367,33 +393,6 @@ long int qtd_divisores(long int a)
 }
 
 
-void numeros_perfeitos(long int a)
-{
-	long int num,soma_div,i;
-
-	num = (potencia(2,(a-1))*(potencia(2,a)-1));
-	//    2^s                2^(s-1)-1               na notação do livro.
-
-
-
-		soma_div=0;
-		for(i=1;i<=num;i++)
-		{
-			if(num%i==0)
-			{
-				soma_div+=i;
-			}
-		}
-
-		if(num*2==soma_div)
-		{
-			printf("\nOba! O resultado = %ld eh um numero perfeito\n",num);
-		}
-		else
-		{
-				printf("\nXii, o resultado = %ld nao eh um numero perfeito\n",num);
-		}
-	}
 
 
 
@@ -439,9 +438,9 @@ float raiz(long int n)
 void print_vetor(long int *pvt, int tam)
 {
     int i;
-	puts("\n{");
-    for(i=1;i<tam;i++)
+	for(i=1;i<tam;i++)
     {
+		if(i==1) printf("\n\t{");
         printf(" %ld ,",pvt[i]);
     }
 	puts("}\n");
@@ -460,3 +459,219 @@ int checa_primalidade(long int n)
 	}
 	return 1;
 }
+
+void teste1(void)
+{
+    int opcao=-1, base;
+    mpz_t numero;
+    mpz_init(numero);
+    puts("\n\n[*~*~*~*~*~*~*~*~*~*~*~*~]\n"
+	"\nO teste de fermat verifica se um numero é composto,\n"
+	"utilizando uma das versões do Pequeno Teorema de Fermat, que afirma:\n\n"
+	"\t\tSe um numero N é primo, qualquer numero A^(N-1) é congruente a 1 módulo N");
+    puts("\n\nInsira o numero que deseja checar se é composto:");
+    gmp_scanf("%Zd",numero);
+
+	puts("\n\nInsira a base A que deseja testar:");
+
+    base = valida_int();
+
+    while(opcao!=0)
+    {
+        if(teste_fermat(numero,base))
+        {
+            gmp_printf("\n\t\t\t\t\t\t\t\tO numero %Zd é pseudoprimo para a base %d\n\n"
+            "Deseja testar outra base? Insira-a, ou 0 para cancelar.\n",numero,base);
+            base=valida_int();
+            opcao=base;
+            
+            
+        }
+        else
+        {
+            gmp_printf("\n\t\t\t\t\t\t\t\tO numero %Zd é composto\n\n",numero);
+            opcao=0;
+   
+        }
+    }
+
+}
+
+int teste_fermat(mpz_t numero, int base_i)
+{
+    mpz_t modulo,base,exp;
+    mpz_init(modulo);
+    mpz_init(exp);
+    mpz_init_set_ui(base,base_i);
+    mpz_sub_ui(exp,numero,1);
+    mpz_powm(modulo,base,exp,numero);
+
+    if(!mpz_cmp_ui(modulo,1)) // caso seja pseudoprimo a base
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void teste2()
+{
+    mpz_t numero;
+    mpz_init(numero);
+	puts("Um numero é considerado de Carmichael se para todas as bases A,\n"
+	"ele satisfaz a seguinte versão do Pequeno Teorema de Fermat:\n"
+	"\n\t Se N é primo, qualquer numero A^N é congruente a A modulo N\n");
+    puts("\nInsira o numero a testar primalidade/carmichael:");
+
+    gmp_scanf("%Zd",numero);
+
+    if(teste_car(numero))
+    {
+        puts("\n\t\t\t\t\t\t\t\tO numero é composto e não-carmichael.\n");
+    }
+    else
+    {
+        puts("\t\t\t\t\t\t\t\t O numero é primo ou carmichael!");
+    }
+
+}
+int teste_car(mpz_t numero)
+{
+    mpz_t iterador,modulo,limite;
+
+    mpz_init_set_ui(iterador,2);
+    mpz_init(modulo);
+    mpz_init(limite);
+    mpz_sub_ui(limite,numero,1);
+
+    while(mpz_cmp(iterador,limite)<0)
+    {
+        gmp_printf("Testando com a base %Zd....\n",iterador);
+        mpz_powm(modulo,iterador,numero,numero);
+
+        if(mpz_cmp(modulo,iterador)) // se o modulo e diferente da base(iterador)
+        {
+            return 1;
+        }
+        mpz_add_ui(iterador,iterador,1);
+    }
+    return 0;
+}
+
+void teste3()
+{
+    int base,opcao=-1;
+    mpz_t numero;
+    mpz_init(numero);
+
+    puts("\n\nQual numero deseja testar?");
+    gmp_scanf("%Zd",numero);
+
+    puts("\n\nCom qual base?");
+    base = valida_int();
+    while(opcao!=0)
+    {
+        if(miller_rabin(numero,base))
+        {
+            gmp_printf("\t\t\t\t\t\t\t\t\t"
+            "O Numero %Zd é composto.\n",numero);
+            break;
+        }
+        else
+        {
+            gmp_printf("\t\t\t\t\t\t\t\t\t"
+            "O numero %Zd é pseudoprimo forte para a base %d \n"
+            "Deseja testar outra base? Insira-a ou 0 para cancelar.\n",numero,base);
+            base=valida_int();
+            opcao = base;
+        }
+    }
+}
+
+int miller_rabin(mpz_t numero,int base_i)
+{
+    mpz_t limite,k,modulo,par,q,i,base;
+    mpz_init_set_ui(i,0);
+    mpz_init(modulo);
+    mpz_init(k);
+    mpz_set_ui(k,0);
+    mpz_init(q);
+
+    mpz_init(limite);
+    mpz_sub_ui(limite,numero,1);
+    mpz_init_set(par,limite);
+
+    mpz_init_set_ui(base,base_i);
+
+    while(1)
+    {
+        if(mpz_even_p(par))
+        {
+            mpz_add_ui(k,k,1);
+            mpz_fdiv_q_ui(par,par,2);
+        }
+        else break;
+    } // pegar q e k
+    mpz_set(q,par);
+    mpz_powm(modulo,base,q,numero);
+
+    // gmp_printf("TEmos q=%Zd e k=%Zd\n"
+    // "i=%Zd e modulo=%Zd\n",q,k,i,modulo);
+
+    while(mpz_cmp(i,k))
+    {
+        if( (!mpz_cmp_ui(i,0) && !mpz_cmp_si(modulo,1)) || ( mpz_cmp_si(i,0) && !mpz_cmp(modulo,limite) ))
+        {
+            return 0; // se pseudoprimo forte a base
+        }
+        mpz_add_ui(i,i,1);
+        mpz_pow_ui(q,modulo,2);
+        mpz_mod(modulo,q,numero);
+
+
+    }
+    return 1; // se composto
+
+
+}
+void exibir_pseudoprimos()
+{
+    int base,n,i,fortes;
+    mpz_t candidato;
+    mpz_init(candidato);
+
+    puts("\n\n Deseja pseudoprimos para qual base?");
+    base = valida_int();
+
+    mpz_set_ui(candidato,base+2);
+    puts("QUantos pseudoprimos?");
+    n = valida_int();
+	puts("Insira:\n [1] Pseudoprimos \"Normais\"\n"
+	"[2]Somente pseudoprimos fortes\n");
+	fortes =valida_int();
+
+
+    for(i=0;i<n;i++)
+    {
+        while(!teste_fermat(candidato,base) || mpz_probab_prime_p(candidato,30))
+        {
+            mpz_add_ui(candidato,candidato,1);
+        }
+        if(fortes==1) gmp_printf("\t\t\t\t\t\t\t\t O numero %Zd é pseudoprimo para %d\n",candidato,base);
+        if(!miller_rabin(candidato,base))
+        {
+            if(fortes==1) gmp_printf("\t\t\t\t\t\t\t\t\tE é forte também.\n");
+			else 
+			{
+				gmp_printf("\t\t\t\t\t\t\t\t\tO numero %Zd é pseudoprimo forte para %d\n",candidato,base);
+			}
+        }
+		else if(fortes==2)
+		{
+			i--;
+		}
+        mpz_add_ui(candidato,candidato,1);
+    }
+
+    
+}
+
