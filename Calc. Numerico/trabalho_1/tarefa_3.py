7b4ea289d10aa0509489ecbeb3405215715bc42f
afonsolpjr <afonsolpjr@hotmail.com>
2023-04-30 17:50:39 -0300

    quase tudo feito!!

---
+++ Calc. Numerico/trabalho_1/tarefa_3.py
@@ -0,0 +1,51 @@
+import  math
+import sys
+
+sys.setrecursionlimit(10000)
+# ITEM 2B
+
+def euler_kahan(n_termo):
+    erro_a_compensar = 0
+    termo = 1
+    soma = 1      #computando termo 0
+    for i in range(1,n_termo):
+
+        soma_antes = soma
+        termo = termo/i
+        termo_compensado = termo + erro_a_compensar        
+        soma += termo_compensado  #compensa erro anterior
+        erro_a_compensar = (soma_antes - soma) + termo_compensado       #calcula do termo efetivamente soma e o termo
+        # print("\nErro a comepnsar = {:.50f}".format(erro_a_compensar) )
+
+
+    return (soma - erro_a_compensar)
+
+def pi_kahan(n_termo):
+    termo = 1
+    erro_a_compensar = 0
+    soma = termo* 2            #computando termo 0, daonde se derivará os outros
+    for i in range(1,n_termo):
+        soma_antes = soma
+        termo = termo * (i/((2*i) + 1))
+        termo_compensado = termo + erro_a_compensar/2           #precisa dividir o erro porque como a soma é += 2* o termo, ele seria compensado 2x
+        soma += termo_compensado*2
+        erro_a_compensar = (soma_antes - soma) + termo_compensado*2
+
+    return soma
+
+
+print("\n\t[CALCULANDO COM SOMA COMPENSADA DE KAHAN....]\n")
+
+lista_k = [5,10,50,100,500,1000,5000]
+
+for x in lista_k:
+    aprox=pi_kahan(x)
+    
+    erro = math.fabs(math.pi - aprox)
+    print("PI recursivo com {} termos : {:.50f}\nErro absoluto de {:.50f}\n".format(x,aprox,erro))
+
+for x in lista_k:
+    aprox = euler_kahan(x)
+    
+    erro = math.fabs(math.e - aprox)
+    print(" 'e' compensado com {} termos : {:.50f}\nErro absoluto de {:.50f}\n".format(x,aprox,erro))
---
+++ POO/lista_4/questoes_teoricas.md
@@ -0,0 +1,14 @@
+# Questões Teóricas
+
+## 1. 
+Uma copia rasa somente copia o valor de referência do objeto a ser copiado, logo, mudanças no "novo" objeto também são refletidas no objeto "original". 
+Uma deep copy copia todos os valores instanciando um novo objeto, sendo um processo mais demorado, mas preserva os valores do objeto "original".
+
+
+## 2. 
+O valor retornado será "False". Ele poderia ser "True" caso a comparação fosse feita com o método "Equals()", propriamente definido para retornar "True" no caso do atributo "valor" ser igual.
+
+## 3. 
+O GC reconhece objetos a serem excluidos pela falta de referência à eles.
+
+
