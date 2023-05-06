810952a57583a0f2d9367d9931824051947442de
afonsolpjr <afonsolpjr@hotmail.com>
2023-05-06 17:36:59 -0300

    fiz uma arv binaria

---
+++ Est. de dados/arvbin.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct __no
+{
+    int chave,nivel;
+    struct __no *esq;
+    struct __no *dir;
+}NO;
+
+NO* busca(NO* raiz, int n)
+{
+    if(raiz==NULL)
+        return NULL;
+
+
+    if(n==raiz->chave)
+        return raiz;
+    else if(n>raiz->chave)
+        return busca(raiz->dir,n);
+    else
+        return busca(raiz->esq,n);
+
+}
+
+void print_preordem(NO *raiz)
+{
+    if(raiz == NULL)
+    {
+        return;
+    }
+    pprintf(" [%d] ",raiz->chave);
+    print_preordem(raiz->esq);
+    print_preordem(raiz->dir);
+
+}
+
+void insere_no(NO **ptr_raiz,int n)
+{
+    NO *raiz = *ptr_raiz;
+    if( raiz == NULL)
+    {
+        *ptr_raiz = raiz = (NO*) malloc(sizeof(NO));
+        raiz->chave = n;
+        raiz->dir = NULL;
+        raiz->esq = NULL;
+        raiz->nivel = 1;
+        return;
+    }
+    if( busca(raiz,n) !=NULL) //sem dupliocatas
+        return;
+    
+
+    
+    if(n > raiz->chave)
+    {
+        insere_no(&(raiz->dir),n);
+    }
+    else
+    {
+        insere_no(&(raiz->esq),n);
+    }
+}
+
+void print_pos_ordem(NO* raiz)
+{
+    if(raiz == NULL)
+        return;
+
+    printf(" [%d] ",raiz->chave);
+    print_pos_ordem(raiz->dir);
+    print_pos_ordem(raiz->esq);
+}
+
+void print_ordem(NO* raiz)
+{
+    if(raiz==NULL)
+        return;
+
+    if( raiz->esq != NULL )
+    {
+        print_ordem(raiz->esq);
+        printf(" [%d] ",raiz->chave);
+        print_ordem(raiz->dir);
+    }
+    else
+    {
+        printf(" [%d] ",raiz->chave);
+        print_ordem(raiz->dir);
+    }
+    return;
+
+
+}
+
+
+
+int main()
+{
+
+    NO *p_raiz = NULL;
+    int chave,i;
+    puts("Insira numeros para insercao na arvore\n");
+    while( (scanf("%d",&chave)) && (chave!= 0) )
+    {
+        insere_no(&p_raiz,chave);
+    }
+
+    printf("\nArvore em pre-ordem : ");
+    print_preordem(p_raiz);
+
+    printf("\nArvore em pos-ordem: ");
+    print_pos_ordem(p_raiz);
+
+    printf("\nArvore em ordem: ");
+    print_ordem(p_raiz);
+    
+    for(i=0;i<5;i++)
+    {
+        printf("\nbuscando valor [%d]",i);
+        if(busca(p_raiz,i)!=NULL)
+        {    
+            printf("\n\tValor [%d] esta na arvore!",i);
+        }
+    }
+    return 0;
+}
