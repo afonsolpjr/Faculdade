7ad3597c78b48c11392ef6f859f8bb7387e270a9
afonsolpjr <afonsolpjr@hotmail.com>
2023-06-13 17:13:22 -0300

    Adaptei a heap para arestas

--- Est. de dados/union-find.c
+++ Est. de dados/union-find.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+/* ESTRUTURAS */
+typedef struct _aresta
+{
+    int v1;
+    int v2;
+    int custo;
+}ARESTA;
 
+typedef struct _node
+{
+    int chave;
+    int custo;
+    struct _node *prox;
+
+}NODE;
 /* FUNÇÕES DA HEAP MINIMA */
 
-int check_node(int *heap, int node) // Alterações a fazer caso queira heap max ou min.
+int check_node(ARESTA *heap, int node) // Alterações a fazer caso queira heap max ou min.
 {
     if(node==0)
     {
@@ -13,7 +27,7 @@ int check_node(int *heap, int node) // Alterações a fazer caso queira heap max
     {
         // <= para heap max
         // >= para heap min
-        if(heap[node] >= heap[(node/2)])
+        if(heap[node].custo >= heap[(node/2)].custo)
             return 1;
         else 
             return 0;
@@ -25,9 +39,10 @@ int check_node(int *heap, int node) // Alterações a fazer caso queira heap max
     }
 }
 
-void subir(int *heap,int alvo)
+void subir(ARESTA *heap, int alvo)
 {
-    int temp,alvo_troca;
+    int alvo_troca;
+    ARESTA temp;
 
     while( alvo>0 && !check_node(heap,alvo))
     {
@@ -43,9 +58,10 @@ void subir(int *heap,int alvo)
     return;
 }
 
-void descer(int *heap,int n, int alvo) // Alterações a fazer caso queira heap max ou min
+void descer(ARESTA *heap,int n, int alvo) // Alterações a fazer caso queira heap max ou min
 {
-    int alvo_troca,temp;
+    int alvo_troca;
+    ARESTA temp;
     if(alvo==0)
     {
     
@@ -58,7 +74,7 @@ void descer(int *heap,int n, int alvo) // Alterações a fazer caso queira heap 
             {
             //caso heap-max = perguntar se é maior
             //caso heap-min = perguntar se é o menor
-                heap[alvo_troca] < heap[alvo_troca+1] ? (alvo_troca) : (alvo_troca++);
+                heap[alvo_troca].custo < heap[alvo_troca+1].custo ? (alvo_troca) : (alvo_troca++);
             }
 
             temp = heap[alvo_troca];
@@ -70,18 +86,18 @@ void descer(int *heap,int n, int alvo) // Alterações a fazer caso queira heap 
     return;
 }
 
-void altera_prioridade(int *heap,int n, int alvo, int p_alvo)
+void altera_prioridade(ARESTA *heap,int n, int alvo, int p_alvo)
 {
     int aumento=0;
 
-    if(p_alvo > heap[alvo])
+    if(p_alvo > heap[alvo].custo)
         aumento=1;
-    if(p_alvo < heap[alvo])
+    if(p_alvo < heap[alvo].custo)
         aumento = 0;
     else
         return; //eh igual neahh
 
-    heap[alvo] = p_alvo;
+    heap[alvo].custo = p_alvo;
 
     while((alvo>0) && (alvo<n) && !check_node(heap,alvo)) //enqnt n satisfazer a propriedade das heaps
     {
@@ -92,20 +108,19 @@ void altera_prioridade(int *heap,int n, int alvo, int p_alvo)
     }
 }
 
-void insere(int *heap, int *n, int novo_p)
+void insere(ARESTA *heap, int m /*n de arestas*/, ARESTA novo_p)
 {
-    heap[(*n)] = novo_p;
-    subir(heap,(*n));
-    (*n)++;
+    heap[m-1] = novo_p;
+    subir(heap,m-1);
 }
 
-void print_heap(int *heap, int n)
+void print_heap(ARESTA *heap, int n)
 {
     int i;
     puts("\n Heap:");
 
     for(i=0;i<n;i++)
-        printf("[%d] ",heap[i]);
+        printf("[%d] ",heap[i].custo);
 
     puts("\n Checaguem da propriedade: ");
     for(i=0;i<n;i++)
@@ -119,16 +134,14 @@ void print_heap(int *heap, int n)
     
 }
 
+void remover(ARESTA *heap,int alvo,int *n)
+{
+    heap[0] = heap[(*n)-1];
+    descer(heap,0)
+
+}
 /* OUTRAS FUNÇÕES */ 
 
-
-typedef struct _node
-{
-    int chave;
-    int custo;
-    struct _node *prox;
-
-}NODE;
 
 int main()
 {
@@ -142,25 +155,40 @@ int main()
     Seguidos de m linhas, dizendo qual os vértices q a aresta liga e o peso 
     */
 
-    int i,n,m, link[2],custo;
+    int i,n,m,v1,v2,custo;
+    ARESTA *arestas,input;
     NODE *lista_adj;
 
     scanf("%d %d", &n,&m);
 
     //printf("\nn = %d    m = %d\n",n,m);
-    lista_adj = (NODE*) calloc(n,sizeof(NODE));
-    
-    if(lista_adj == NULL)
-        return 1;
+    //lista_adj = (NODE*) calloc(n,sizeof(NODE));
+
+    arestas = (ARESTA*) calloc(m,sizeof(ARESTA));
+
 
     i=0;
     while(i<m)
     {
-        scanf("%d %d %d",link,&(link[1]),&custo);
+        scanf("%d %d %d",&v1,&v2,&custo);
         // printf("\n\t%d %d %d\n", link[0],link[1],custo);
+        input.v1=v1;
+        input.v2=v2;
+        input.custo = custo;
         i++;
-        
-        //criar lista de adjacencias
+        insere(arestas,i,input);
     }
 
+    print_heap(arestas,m);
+    // for(i=0;i<m;i++)
+    //     printf("Aresta %d = %d %d %d\n",i,arestas[i].v1, arestas[i].v2,arestas[i].custo);
 }
+
+
+/*
+5 4
+0 1 5
+0 2 3
+2 3 1
+4 3 2
+*/
