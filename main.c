b61d5607da7c5ac42bdd98348398305e4d615f3b
afonsolpjr <afonsolpjr@hotmail.com>
2024-06-28 11:49:51 -0300

    - trabalho de lf mto bom quase acabado

--- main.c
+++ main.c
@@ -55,53 +55,12 @@ UNION
 Exemplos de mensagens de erro
 ------------------------------
 
-(a
+(a)
 Erro de sintaxe na posição 3: esperava ')', encontrei '\n'
 
-(a))
+(a)
 Erro de sintaxe na posição 4: esperava '\n', encontrei ')'
 
-
-Representação da árvore em C
-----------------------------
-
-Use a seguinte estrutura de dados para a sua árvore
-
-
-    enum RegExpTag {
-        TAG_EMPTY,
-        TAG_CHAR,
-        TAG_STAR,
-        TAG_CONCAT,
-        TAG_UNION,
-    };
-
-    struct RegExp {
-        char tag;
-        union {
-            struct {
-                char c;
-            } ch;
-            struct {
-                struct RegExp *filho;
-            } un;
-            struct {
-                struct RegExp *filho1;
-                struct RegExp *filho2;
-            } bin;
-        } u;
-    };
-
-    typedef struct RegExp RegExp;
-
-
-Implemente os seguintes construtores para nós de árvore:
-
-    RegExp *new_empty();
-    RegExp *new_char(char c);
-    RegExp *new_star(RegExp *filho);
-    RegExp *new_concat(RegExp *filho1, RegExp *filho2);
-    RegExp *new_union(RegExp *filho1, RegExp *filho2);
 
 
 Gramática das expressões regulares
@@ -161,6 +120,8 @@ Pseudocódigo:
 #include <stdlib.h>
 #include <ctype.h>
 
+
+/* Definição dos Tipos */
 enum RegExpTag {
     TAG_EMPTY,
     TAG_CHAR,
@@ -168,7 +129,6 @@ enum RegExpTag {
     TAG_CONCAT,
     TAG_UNION,
 };
-/* Definição dos Tipos */
 typedef struct {
     char c;
 }Char;
@@ -237,101 +197,12 @@ RegExp *newUnion(RegExp *filho1, RegExp *filho2){
     return exp;
 }
 
-/*
-Grammar notation	Code representation
-Terminal	            Code to match and consume a token
-Nonterminal	            Call to that rule’s function
-|	            if or switch statement
-* or +	                while or for loop
-?	                if statement
-*/ 
-
+/*Variáveis globais */
 char c;
-int p;
-int cp; /* contagem de parenteses*/
-void consume()
-{
-    c=getchar();
-    p++;
-}
-static RegExp* parseRegexp();
-static RegExp* parseBasic(){
-    RegExp *e;
-    if(c!='(' && c!=')')
-    {
-        e =  newChar(c);
-        consume();
-    }
-    else if(c=='(')
-    {
-        consume();
-        cp++;
-        e = parseRegexp();
-        if(c!=')')
-        {
-            puts("\nerro!! esperava um parenteses");
-            exit(1);
-        }
-        else
-        {
-            consume();
-            cp--;
-        }
-    }
-    return e;
-}
-static RegExp* parseStar(){
-    RegExp *e1;
+int p,cp,line; /* Posição, contagem de parênteses e linha*/
 
-    e1 = parseBasic();
-
-    while(c=='*'){
-        consume();
-        e1 = newStar(e1);
-    }
-    return e1;
-}
-static RegExp* parseConcat(){
-    RegExp *e1,*e2;
-    
-    switch(c){
-    case ')':
-        if(cp==0)
-        {
-            printf("\n\terro! nao esperava parenteses");
-            exit(1);
-        }
-    case '\n':
-    case '\0':
-    case '|':
-        return newEmpty();
-    }
-    
-    e1 = parseStar();
-    e2 = parseConcat();
-
-    if(e2->tag!=TAG_EMPTY)
-        return newConcat(e1,e2);
-    else
-        return e1;
-}
-static RegExp* parseUnion(){
-    RegExp *e1,*e2;
-    e1 = parseConcat();
-    if(c=='|'){
-        consume();
-        e2 = parseUnion();
-        return newUnion(e1,e2);
-    }
-    
-    return e1;
-}
-static RegExp* parseRegexp(){
-    return parseUnion();
-}
-
+/* Funções auxiliares */
 const char *types[] = {"EMPTY", "CHAR","STAR","CONCAT", "UNION"};
-
 void printIndent(int lvl)
 {
     int i =0;
@@ -342,33 +213,7 @@ void printIndent(int lvl)
 }
 void printTree(RegExp *tree, int indent_lvl)
 {
-   /* Imprimindo uma árvore
-    ---------------------
-
-    Implemente uma função recursiva que recebe uma árvore e a imprime em stdout.
-    Na primeira linha, imprima a tag do nó (e, no caso CHAR também imprima o char).
-    Nas linhas subsequentes, imprima recursivamente os nós filhos, indentados por
-    dois espaços. (Veja os exemplos de entradas e saídas) */
-//     Exemplo de saída
-// ----------------
-
-// CONCAT
-//   CHAR a
-//   CONCAT
-//     CHAR b
-//     STAR
-//       STAR
-//         CHAR c
-// UNION
-//   CONCAT
-//     CHAR a
-//     CHAR b
-//   UNION
-//     EMPTY
-//     CHAR c
-    int i;
-
-    printf("%s",types[tree->tag]);
+    printf("%s",types[(int)tree->tag]);
 
     switch(tree->tag)
     {
@@ -396,43 +241,147 @@ void printTree(RegExp *tree, int indent_lvl)
     }
     return;
 }
+void consume()
+{
+    c=getchar();
+    p++;
+}
+void printError()
+{
+    printf("Erro na linha %d, caractere na posicao %d:",line+1,p);
+}
 
+/* Parseadores por tipos*/
+static RegExp* parseRegexp();
+static RegExp* parseBasic(){
+    RegExp *e;
+    
+    if(c=='(')
+    {
+        consume();
+        cp++;
+        e = parseRegexp();
+        if(c!=')')
+        {
+            printError();
+            printf("\n\tFim de linha inesperado. (Esperando fechamento de parênteses)");
+            exit(1);
+        }
+        else
+        {
+            consume();
+            cp--;
+        }
+    }
+    else
+    {
+        e =  newChar(c);
+        consume();
+    }
+    return e;
+}
+static RegExp* parseStar(){
+    RegExp *e1;
+
+    e1 = parseBasic();
+
+    while(c=='*'){
+        consume();
+        e1 = newStar(e1);
+    }
+    return e1;
+}
+static RegExp* parseConcat(){
+    RegExp *e1,*e2;
+    
+    switch(c){
+    case ')':
+        if(cp==0)
+        {
+            consume();
+            printError();
+            printf("\n\tCaractere ')' não esperado.");
+            exit(1);
+        }
+        [[fallthrough]];
+    case '\n':
+    case '\0':
+    case '|':
+        return newEmpty();
+    }
+    
+    e1 = parseStar();
+    e2 = parseConcat();
+
+    if(e2->tag!=TAG_EMPTY)
+        return newConcat(e1,e2);
+    else
+        return e1;
+}
+static RegExp* parseUnion(){
+    RegExp *e1,*e2;
+
+    if(c=='*'){
+        consume();
+        printError();
+        printf("\n\tCaractere '*' não permitido em inicio de expressoes.");
+        exit(1);
+    }
+    e1 = parseConcat();
+    if(c=='|'){
+        consume();
+        e2 = parseUnion();
+        return newUnion(e1,e2);
+    }
+    
+    return e1;
+}
+static RegExp* parseRegexp(){
+    return parseUnion();
+}
+static RegExp* parseLine(){
+    RegExp *e;
+    e = parseRegexp();
+    if(c=='\n')
+        consume();
+    return e;
+}
 
 int main()
 {
-    RegExp *exp;
+    RegExp **expressions;
+    int max_exp,i;
+
     p=0;
-    cp=0;
+    max_exp = 4;
+    line = 0 ;
+
+    expressions = malloc(sizeof(max_exp*sizeof(RegExp*)));
+
     consume();
-    exp = parseRegexp();
-    printTree(exp,0);
+    while(c!=EOF){
+
+        if(line==max_exp){
+            max_exp*=2;
+            expressions = realloc(expressions,max_exp*sizeof(RegExp*));
+        }
+        cp=0;
+        p=0;
+        expressions[line] = parseLine();
+
+        line++;
+    }
+    for(i=0;i<line;i++)
+    {
+        printTree(expressions[i],0);
+        printf("\n");
+    }
+
     return 0;
 }
 
-
-
-
 /* A FAZER:
 
-    - Indicar erro e posicao do erro na linha
-    - ler mais de uma linha de regex
-
-obs:
-QUal deve ser o comportamento caso ache uma estrela sem nada antes? ou seja, logo no começo ou depois de ( e |?
-
-A entrada '**' está gerando:
-
-STAR
-  CHAR *
-
-Outra entrada similar: **|*, gera:
-
-UNION
-  STAR
-    CHAR *
-  CHAR *
-
-
-
-
+    - Imprimir erros em stderr
+        erros localizados no parse union, parseconcat, e parsebasic
 */
