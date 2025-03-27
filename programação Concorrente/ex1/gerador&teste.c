3dad6d1fedf28e3bee852bdcb8b1d917b1b27c08
afonsolpjr <afonsolpjr@hotmail.com>
2025-03-27 17:54:56 -0300

    exercicio1 de prog concorrente

---
+++ programação Concorrente/ex1/gerador&teste.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/* 
+1. Comece gerando os casos de teste (vetores de entrada e resultado esperado). Es-
+creva um programa sequencial em C que gere dois vetores de entrada (tipo: float)
+de dimensao ̃ N, com valores randomicos (gere numeros fracion  ́ arios).
+Escreva em um arquivo binario o valor de N (tipo: inteiro) e os dois vetores. Depois calcule
+o produto interno desses dois vetores e escreva o resultado encontrado no mesmo
+arquivo binario (tipo: double).  ́
+*/
+
+
+/*
+    @brief Retorna um vetor de tamanho @tam_vetor com valores quase-aleatórios em float.
+    @param vetor ponteiro para o vetor de floats
+    @param tam_vetor tamanho do vetor
+*/
+void vetor_float_aleatorio(float *vetor,int tam_vetor){
+    float num_aleatorio;
+    int i;
+    for(i=0;i<tam_vetor;i++){
+        num_aleatorio = 5.0/(rand()%500+1)*100;
+        if(rand()%2)
+            num_aleatorio*=-1;
+        vetor[i]=num_aleatorio;
+        printf(" %f ",num_aleatorio);
+    }
+}
+
+/* 
+ @brief calcula produto interno entre 2 vetores
+ @param v1 primeiro vetor
+ @param v2 segundo vetor
+ @param n tamanho dos vetores
+ @return produto interno de v1 e v2
+*/
+float produto_interno_seq(float *v1,float *v2,int n){
+    int i;
+    float resultado=0;
+    for(i=0;i<n;i++)
+        resultado+=(v1[i]*v2[i]);
+    return resultado;
+}
+
+int main(int argc, char const *argv[])
+{
+    int tam_vetor,i;
+    float *vetor[2];
+    double produto_interno;
+    FILE * ptr_arquivo;
+    srand(time(NULL));
+
+    /* checando argumentos do programa */
+    if(argc!=3){
+        printf("Por favor, passe o tamanho do vetor e o nome do arquivo binário como (únicos) argumentos."
+            "\n\t./<programa> <tamanho do vetor> <nome do arquivo binario>\n");
+        return 1;
+    }
+    tam_vetor = atoi(argv[1]);
+
+    /*Alocando vetor*/
+    for(i=0;i<2;i++)
+        vetor[i] = (float*) malloc(sizeof(float)*tam_vetor);
+    if(!vetor[0] || !vetor[1]){
+        puts("Houve um erro na alocação dos vetores");
+        return 2;
+    }
+        
+        /* Populando e printando vetor */
+    for(i=0;i<2;i++){
+        printf("\tVETOR %d\n[",i+1);
+        vetor_float_aleatorio(vetor[i],tam_vetor);
+        printf("]~~~\n\n");
+    }
+    
+    /* Calculando produto interno */
+    produto_interno=produto_interno_seq(vetor[0],vetor[1],tam_vetor);
+    printf("\n\tProduto interno = %f\n",produto_interno);
+
+    /* Criando arquivo binário */
+    ptr_arquivo = fopen(argv[2],"w");
+    fwrite(&tam_vetor,sizeof(int),1,ptr_arquivo);
+    fwrite(vetor[0],sizeof(float),tam_vetor,ptr_arquivo);
+    fwrite(vetor[1],sizeof(float),tam_vetor,ptr_arquivo);
+    fwrite(&produto_interno,sizeof(double),1,ptr_arquivo);
+    
+    
+    fclose(ptr_arquivo);
+    return 0;
+}
---
+++ programação Concorrente/ex1/produto_interno.c
@@ -0,0 +1,164 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+/*3. Escreva um programa concorrente em C que recebe como entrada o numero de  ́
+threads T e um nome de arquivo, carregue desse arquivo a dimensao ̃ N e dois
+vetores de entrada, execute o calculo do produto interno desses dois vetores di-  ́
+vidindo a tarefa entre as T threads de forma balanceada, e ao final compare o
+valor calculado com o valor registrado no arquivo de entrada.
+*/
+
+typedef struct{
+    float *v1,*v2;
+    int n,id;
+} prod_int_parcial_targs;
+
+/**
+ * @brief Printa os dados lidos (sanity check kkkkk)
+ * @param tam_vetor 
+ * @param vetores 
+ * @param prod_interno 
+ */
+void printa_dados_lidos(int tam_vetor,float **vetores,float prod_interno){
+    int i,j;
+    printf("\nDados lidos:"
+        "\n\ttamanho de vetor:%d"
+        "\n\tproduto_interno=%f\n",tam_vetor,prod_interno);
+
+    for(i=0;i<2;i++){
+        printf("Vetor %d\n[",i);
+        for ( j = 0; j < tam_vetor; j++)
+        {
+            printf(" %f ",vetores[i][j]);
+        }
+        puts(" ]");
+    }
+}
+
+/**
+ * @brief Calcula o produto interno parcial (de n elementos) dos dois vetores dados
+ * @param arg struct contendo vetores, e numero de elementos a serem processados
+ * @return struct contendo
+ */
+void *prod_interno_parcial(void *arg)
+{
+    prod_int_parcial_targs *argumentos;
+    double *retorno;
+    int i;
+
+    retorno = (double*) malloc(sizeof(double));
+    argumentos = (prod_int_parcial_targs*) arg;
+    *retorno = 0;
+    
+    // printf("\t\tThread %d começando\n",argumentos->id);
+    for(i=0;i<argumentos->n;i++)
+        *retorno += (argumentos->v1[i] * argumentos->v2[i]);
+
+    // printf("\t\t\tresultado parcial dentro da thread %d: %f\n",argumentos->id,*retorno);
+    free(arg);
+    pthread_exit((void*) retorno);
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    FILE * ptr_arqv;
+    int i,tam_vetor,n_threads,posicao_comeco,erro;
+    float *vetores[2];
+    double prod_interno_seq,*retorno,prod_int_concorrente;
+    prod_int_parcial_targs *args;
+    pthread_t *t_ids;
+    
+    /* Validar argumentos*/
+    if(argc!=3){
+        printf("Por favor, passe o numero de threads e o nome do arquivo binário como (únicos) argumentos."
+            "\n\t./<programa> <numero de threads> <nome do arquivo binario>\n");
+        return 1;
+    }
+    n_threads=atoi(argv[1]);
+    if(!n_threads){
+        printf("\n Insira um numero de threads válido. [o numero inserido foi %d]",n_threads);
+        return 2;
+    }
+        /*le tamanho do vetor*/
+    ptr_arqv = fopen(argv[2],"r");
+    fread(&tam_vetor,sizeof(int),1,ptr_arqv);
+    
+    if(n_threads>tam_vetor){
+        printf("O numero de threads indicado (%d) é maior que o tamanho do vetor (%d)."
+            " O programa será encerrado\n",n_threads,tam_vetor);
+    }
+    
+    /* Ler arquivo binario, inicializando vetores:
+        conteúdo:tam,vetor1,vetor2,produto_interno */
+    for(i=0;i<2;i++)
+        vetores[i] = (float*) malloc(sizeof(float)*tam_vetor);
+    if(!vetores[0] || !vetores[1]){
+        puts("Houve um erro na alocação dos vetores");
+        return 3;
+    }
+        
+    fread(vetores[0],sizeof(float),tam_vetor,ptr_arqv);
+    fread(vetores[1],sizeof(float),tam_vetor,ptr_arqv);
+    fread(&prod_interno_seq,sizeof(double),1,ptr_arqv);
+
+    // printa_dados_lidos(tam_vetor,vetores,prod_interno_seq);
+        
+    /* Estratégia para divisão da carga:
+        - Cada thread receberá aproximadamente n=tam_vetor/n_threads elementos a processar o produto */
+
+    /* argumento para cada thread: 
+        - Endereço da posição que ela deve começar a processar, pros dois vetores
+        - Número de elementos que ela deve processar
+    */
+
+    /*O LOOP: Criando threads*/
+
+    t_ids = (pthread_t*) malloc(sizeof(pthread_t)*n_threads);
+    if(!t_ids){
+        puts("Problema na alocação de vetores para IDs de threads");
+        return 3;
+    }
+    posicao_comeco=0;
+    for(i=0;i<n_threads;i++){
+        /*Criando argumento*/
+        args = (prod_int_parcial_targs*) malloc(sizeof(prod_int_parcial_targs));
+        if(!args){
+            printf("Problemas na alocação dos argumentos\n");
+            return 3;
+        }
+        args->id = i; 
+        // printf("criando thread %d\n",i);
+        args->n = tam_vetor/n_threads;
+        if(i<(tam_vetor%n_threads))
+            args->n++;
+        args->v1 = &vetores[0][posicao_comeco];
+        args->v2 = &vetores[1][posicao_comeco];
+        posicao_comeco += args->n;
+        /*criando threads*/
+        erro = pthread_create(&t_ids[i],NULL,prod_interno_parcial,(void*) args);      
+        if(erro){
+            printf("\nErro [%d] na criação de threads",erro);
+            return 4;
+        }  
+    }
+
+
+    /*
+    Retornando threads
+    */
+    prod_int_concorrente=0;
+    for(i=0;i<n_threads;i++){
+        if(pthread_join(t_ids[i],(void**) &retorno)){
+            printf("Erro no retorno da thread %d\n",i);
+        }
+        // printf("RETORNO da thread %d: %f \n",i,*retorno);
+        prod_int_concorrente += (*retorno);
+        free(retorno);
+    }
+    printf("\nResultado Concorrente: %f\n"
+        "Resultado Sequencial: %f\n"
+        "DIferença absoluta: %f\n",prod_int_concorrente,prod_interno_seq,
+        fabs(prod_int_concorrente-prod_interno_seq));
+    return 0;
+}
