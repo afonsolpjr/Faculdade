f4ddd3616c9ab7482fa8b5cfa23646ca95565783
afonsolpjr <afonsolpjr@hotmail.com>
2025-04-11 06:51:40 -0300

    mudando script progconc

--- programação Concorrente/lab3/ativ1/run.sh
+++ programação Concorrente/lab3/ativ1/run.sh
@@ -10,47 +10,17 @@ else
 fi
 echo -e "\n\tNumero máximo de threads selecionado = $MAX_T\n"
 
-#  objetivo:
-    # executar o programa 10 (ou n vezes) com as configurações abaixo:
-    # - execuções sequenciais:
-        # - dimensoes 500,1000 e 2000
-        # - flags de compilacao −O0, −O1, −O2 e −O3.
-    # - execucoes concorrentes:
-        # - dimensoes 500 1000 e 2000 (usarei 4 threads em todas)
-gcc -o bin_gen -Wall bin_gen.c
-
-#Criando csv e colunas
-COLUNAS="\"tipo_execucao\",\"flags\",\"n_threads\",\"dimensao\",\"t_inicializacao\",\"t_processamento\",\"t_finalizacao\""
-if [ ! -f execucoes.csv ]; then
-    echo $COLUNAS >> execucoes.csv
-fi
-
-DIMENSOES=(500 1000 2000 5000 10000 20000)
-# Gerar dados caso não haja
-if [ ! -f "matvet500.bin" ]; then
-    for dim in "${DIMENSOES[@]}";do
-        ./bin_gen $dim $dim "matvet$dim.bin";
-    done
-    echo -e "\n\tDados gerados!";
-fi
-
-TIPOS_EXECUCAO=(2)
-FLAGS=("-O0" "-O1" "-O2"  "-O3")
-
-MELHOR_FLAG=''
-
 
 atividade1(){
     tipo_execucao=2
+    echo -e "\n Analisando melhor flag...\n"
     for flag in "${FLAGS[@]}";do
 
                 COMANDO="gcc $flag -o matvet -Wall matvet.c";
-                echo -e "\nCompilando com flag $flag";
                 $COMANDO;
-                echo -e "\tcalculando para dimensoes "
 
                 for dim in "${DIMENSOES[@]}";do
-                    echo -n " $dim...";
+                    printf "\rFlag: %s | Dimensão %d" "$flag" "$dim"
                     # programa <nome binario> <tipo execucao> <n threads>
                     NOME_BIN="matvet$dim.bin"
                     COMANDO="./matvet $NOME_BIN $tipo_execucao 1 "
@@ -66,17 +36,17 @@ atividade1(){
     echo -e "\n\tFlag com o menor tempo médio geral: $MELHOR_FLAG"
 }
 
-
 atividade2(){
     COMANDO="gcc $MELHOR_FLAG -o matvet -Wall matvet.c";
     $COMANDO;
-
+    echo -e "\n Gerando dados para....\n"
     for dim in "${DIMENSOES[@]}";do
 
         for n_threads in $(seq 2 $MAX_T);do
             # programa <nome binario> <tipo execucao> <n threads>
             NOME_BIN="matvet$dim.bin"
             COMANDO="./matvet $NOME_BIN $tipo_execucao $n_threads"
+            printf "\rDimensao: %d | Numero de Threads: %d" "$dim" "$n_threads"
 
             for ((i=1;i<=10;i++));do
                 OUTPUT=$($COMANDO)
@@ -91,7 +61,37 @@ atividade2(){
 
 }
 
+#  objetivo:
+    # executar o programa 10 (ou n vezes) com as configurações abaixo:
+    # - execuções sequenciais:
+        # - dimensoes 500,1000 e 2000
+        # - flags de compilacao −O0, −O1, −O2 e −O3.
+    # - execucoes concorrentes:
+        # - dimensoes 500 1000 e 2000 (usarei 4 threads em todas)
+gcc -o bin_gen -Wall bin_gen.c
 
+#Criando csv e colunas
+COLUNAS="\"tipo_execucao\",\"flags\",\"n_threads\",\"dimensao\",\"t_inicializacao\",\"t_processamento\",\"t_finalizacao\""
+if [ ! -f execucoes.csv ]; then
+    echo $COLUNAS >> execucoes.csv
+fi
+
+DIMENSOES=( 500 1000 2000 5000 10000 20000 )
+# Gerar dados caso não haja
+if [ ! -f "matvet500.bin" ]; then
+    echo -e "\ngerando dados de dimensões..."
+    for dim in "${DIMENSOES[@]}";do
+        echo -n "$dim..."
+        ./bin_gen $dim $dim "matvet$dim.bin";
+    done
+
+    echo -e "\n\tDados gerados!";
+fi
+
+TIPOS_EXECUCAO=(2)
+FLAGS=("-O0" "-O1" "-O2"  "-O3")
+
+MELHOR_FLAG=''
 
 echo -e "\nQual atividade executar? (1,2,3,4)\n"
 read -p "Insira o numero: " ATIVIDADE
