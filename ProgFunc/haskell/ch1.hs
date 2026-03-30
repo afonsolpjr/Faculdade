2246dbf39052382ff601228ca2f6db1b3fca69d3
afonsolpjr <102331085+afonsolpjr@users.noreply.github.com>
2026-03-30 10:00:58 -0300

    ch1 draft

---
+++ ProgFunc/haskell/ch1.hs
@@ -0,0 +1,34 @@
+  1 {-
+  2 
+  3 2.
+  4 
+  5         (2^3)*4
+  6         (2*3)+(4*5)
+  7         2+(3*(4^5))
+  8 
+  9 3.
+ 10 3.1 begin with lowercase
+ 11 3.2 wrong identation 
+ 12 3.3 what was the 3rd error?
+ 13 -}
+ 14 
+ 15 n = a `div` length xs
+ 16   where
+ 17     a = 10
+ 18     xs = [1,2,3,4,5]
+ 19 
+ 20 
+ 21 {-
+ 22  
+ 23 4.
+ 24 -}
+ 25 
+ 26 last' :: [a] -> a
+ 27 
+ 28 last' [a] =
+ 29 
+~                                                                             
+~                                                                             
+~                                                                             
+~                                                                             
+~                                
