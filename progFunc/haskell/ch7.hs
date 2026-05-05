bbf67ec2351037ca81a7db87222cf301718ff5b6
afonsolpjr <afonsolpjr@hotmail.com>
2026-05-05 05:21:45 -0300

    working on ch7

--- progFunc/haskell/ch7.hs
+++ progFunc/haskell/ch7.hs
@@ -11,7 +11,9 @@ mapr f (x:xs) = ((f x):(mapr f xs))
 filter' :: ( a -> Bool ) -> [a] -> [a]
  
 filter' f xs = [ x | x <- xs, f x ]
- 
+
+-- 
+
 filterr :: ( a -> Bool) -> [a] -> [a]
  
 filterr f [] = []
@@ -30,3 +32,49 @@ filterr f (x:xs)
 --  3. uma lista
 -- Ao passar só dois parâmetros, o retorno será uma função do tipo:
 -- Foldable t => t a -> b
+--
+
+flen :: [a] -> Int
+flen    = foldr (\_ -> (+1)) 0
+
+--rev' :: [a] -> [a]
+--rev' []       = []
+--rev' (x:xs)   = rev' (x:xs)
+
+
+-- Perdi muito tempo tentando entender porque xs:x não funcionava
+-- e porque eu tinha que fazer xs ++ [x].
+-- A explicação está na definição da cons.
+-- (:) :: a -> [a] -> [a]
+
+rev :: [a] -> [a]
+rev     = foldr ( \x xs -> xs++[x] ) []
+
+
+-- foldL !!!!!!!!!!!!!!!!!!!!
+-- Parece codigo imperativo imperativo, mas não.. 
+-- Nesta implementacao de 'sum', usamos um acumulador.
+
+-- foldl :: Foldable t => ( b -> a -> b) -> b -> t a -> b
+sumacc :: Num a => [a] -> a
+sumacc  = acc 0 -- sou um acumulador e quero uma lista, começando com 0
+  where
+    acc n []        = n -- nao tendo mais ng, eu paro
+    acc n (x:xs)    = acc (n+x) xs -- pego o da frente e continuo
+
+
+-- E agora.... veja com foldl
+sumac :: Num a => [a] -> a
+
+sumac = foldl (+) 0 -- ( ˶°ㅁ°)!! 
+
+-- Vou tentar cobrir os casos que vimos no foldr
+
+revl :: [a] -> [a]
+revl    = foldl (\acc elem -> elem:acc ) []
+
+
+
+
+
+
